ab1776ed8bf2b0727cfa5e91b04a1965ea272897
Andrew-DungLe <ddle.2015@smu.edu.sg>
2019-01-25 17:49:22 +0800

    Modify the evaluation function 	modified:   Indexable-Representation.ipynb 	modified:   Matrix-Factorization-Recommendation-Retrieval.ipynb 	modified:   Scalable-Recommendation-Retrieval-with-Locality-Sensitive-Hashing.ipynb 	modified:   utils/evaluation.py

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -1,39 +1,61 @@
 import numpy as np
 from .lsh import * 
 
-def evaluate_topK(test_data, rec_list, k=10):
-    inv_lst = np.unique(test_data[:, 0])
-
+def evaluate_topK(test_data, data, queries, topK=10):
+    inv_list = np.unique(test_data[:, 0]).astype(int)
+    inv_list = [x for x in inv_list if x <= queries.shape[0]]
+    rec_list = {}
+    for inv in inv_list:
+        if rec_list.get(inv, None) is None:
+            rec_list[inv] = np.argsort(np.dot(data, queries[int(inv-1), :]))[-topK:]
+    
     intersection_cnt = {}         
     for i in range(test_data.shape[0]):
         id = int(i)
-        if int(test_data[id, 1]) in rec_list[int(test_data[id, 0])]:
-            intersection_cnt[test_data[id, 0]] = intersection_cnt.get(test_data[id, 0], 0) + 1
+        if int(test_data[id, 0]) in inv_list:
+            if int(test_data[id, 1]) in rec_list[int(test_data[id, 0])]:
+                intersection_cnt[test_data[id, 0]] = intersection_cnt.get(test_data[id, 0], 0) + 1
+                
     invPairs_cnt = np.bincount(np.array(test_data[:, 0], dtype='int32'))
 
     precision_acc = 0.0
     recall_acc = 0.0
-    for inv in inv_lst:
-        precision_acc += intersection_cnt.get(inv, 0) / float(k)
+    for inv in inv_list:
+        precision_acc += intersection_cnt.get(inv, 0) / float(topK)
         recall_acc += intersection_cnt.get(inv, 0) / float(invPairs_cnt[int(inv)])
 
-    return precision_acc / len(inv_lst), recall_acc / len(inv_lst)
+    return precision_acc / len(inv_list), recall_acc / len(inv_list)
 
 
-def evaluate_LSHTopK(test_vec, data, queries, hashFamily, num_table, num_bit, metric, topK):
+def evaluate_LSHTopK(test_data, data, queries, hashFamily, num_table, num_bit, metric, topK):
     lsh_index = LSHIndex(hash_family = hashFamily(data.shape[1]), k = num_bit, L=num_table)
               
     #build index
     lsh_index.index(data.tolist())
 
-    inv_lst = np.unique(test_vec[:, 0]).astype(int)
+    inv_list = np.unique(test_data[:, 0]).astype(int)
+    inv_list = [x for x in inv_list if x <= queries.shape[0]]
 
-    lsh_rec_list = {}
-    for inv in inv_lst:
-        if lsh_rec_list.get(inv, None) is None:
-            lsh_rec_list[inv] = list(map(itemgetter(0), lsh_index.query(queries[inv-1, :], metric, topK)))
+    rec_list = {}
+    for inv in inv_list:
+        if rec_list.get(inv, None) is None:
+            rec_list[inv] = list(map(itemgetter(0), lsh_index.query(queries[inv-1, :], metric, topK)))
+            
+    intersection_cnt = {}         
+    for i in range(test_data.shape[0]):
+        id = int(i)
+        if int(test_data[id, 0]) in inv_list:
+            if int(test_data[id, 1]) in rec_list[int(test_data[id, 0])]:
+                intersection_cnt[test_data[id, 0]] = intersection_cnt.get(test_data[id, 0], 0) + 1
+                
+    invPairs_cnt = np.bincount(np.array(test_data[:, 0], dtype='int32'))
 
-    lsh_prec,lsh_recall = evaluate_topK(test_vec, lsh_rec_list, topK)
+    precision_acc = 0.0
+    recall_acc = 0.0
+    for inv in inv_list:
+        precision_acc += intersection_cnt.get(inv, 0) / float(topK)
+        recall_acc += intersection_cnt.get(inv, 0) / float(invPairs_cnt[int(inv)])
+    
     touched = float(lsh_index.get_avg_touched())/data.shape[0]
     
-    return lsh_prec, lsh_recall, touched
+    return precision_acc / len(inv_list), recall_acc / len(inv_list), touched
